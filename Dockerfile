ddbcc03c37ae5afe472e2fee21d5c7330d96bdb9
David Figueroa <davidfiguerao0327@gmail.com>
2024-05-18 10:02:41 -0500

    setting up docker and baselime

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:22
+
+RUN mkdir -p /app
+WORKDIR /app
+
+COPY package.json .
+COPY package-lock.json .
+
+RUN npm ci
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
---
+++ src/tracing.cjs
@@ -0,0 +1,9 @@
+const { BaselimeSDK } = require("@baselime/node-opentelemetry");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+
+const sdk = new BaselimeSDK({
+  instrumentations: [getNodeAutoInstrumentations()],
+  service: "pandabot",
+});
+
+sdk.start();
